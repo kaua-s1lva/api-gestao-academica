606a2544c22b9a5f8f2a719ca1ee569dd2adfb0d
Vinicius <viniciusufes19@gmail.com>
2026-02-15 19:54:30 -0300

    adicionados logs de rastreabilidade nos métodos do StudentService

--- src/main/java/br/ufes/ccens/service/StudentService.java
+++ src/main/java/br/ufes/ccens/service/StudentService.java
@@ -3,6 +3,8 @@ package br.ufes.ccens.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.jboss.logging.Logger;
+
 import br.ufes.ccens.dto.request.SaveStudentRequest;
 import br.ufes.ccens.entity.StudentEntity;
 import br.ufes.ccens.exception.StudentNotFoundException;
@@ -14,6 +16,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 public class StudentService {
     private final StudentRepository studentRepository;
     private final StudentMapper studentMapper;
+    private static final Logger LOG = Logger.getLogger(StudentService.class);
 
     public StudentService(StudentRepository studentRepository, StudentMapper studentMapper) {
         this.studentRepository = studentRepository;
@@ -24,27 +27,33 @@ public class StudentService {
         try {
             var studentEntity = studentMapper.toEntity(studentRequest);
             studentRepository.persistAndFlush(studentEntity);
+            LOG.info("Estudante criado com sucesso: " + studentEntity.getStudentId());
             return studentEntity;
         } catch (Exception e) {
+            LOG.error("Falha ao criar estudante: " + e.getMessage());
             throw new RuntimeException("Erro ao criar usuário: " + e.getMessage());
         }
     }
 
     public List<StudentEntity> listAll(Integer page, Integer pageSize, String name) {
         if (name != null && !name.isBlank()){
+            LOG.info("Buscando estudante pelo nome: " + name);
             return studentRepository.findByName(name, page, pageSize);
         }
+        LOG.info("Listando todos os estudantes");
         return studentRepository.findAll()
             .page(page, pageSize)
             .list();
     }
 
     public StudentEntity findById(UUID studentId) {
+        LOG.info("Buscando estudante pelo ID: " + studentId);
         return studentRepository.findByIdOptional(studentId)
             .orElseThrow(StudentNotFoundException::new);
     }
 
     public StudentEntity updateStudent(UUID studentId, SaveStudentRequest studentRequest) {
+        LOG.info("Atualizando dados do estudante ID: " + studentId);
         var student = findById(studentId);
         
         var studentEntity = studentMapper.toEntity(studentRequest);
@@ -61,6 +70,7 @@ public class StudentService {
     }
 
     public void deleteStudent(UUID studentId) {
+        LOG.info("Solicitação de exclusão para o estudante ID: " + studentId);
         var student = findById(studentId);
         studentRepository.deleteById(student.getStudentId());
     }
